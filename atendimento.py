0c5e89839ea78ac9dacda6434abf80775dbe1a31
IanKilwiny <ribeiroian083@gmail.com>
2024-09-10 13:52:54 -0300

    Adicionado lista de telefone

--- atendimento.py
+++ atendimento.py
@@ -24,6 +24,15 @@ class Atendimento:
         self.data_do_atendimento = data_do_atendimento
 
 
+# adicionar lista telefônica
+def adicionar_telefone():
+    lista_telefone = []
+    qtd = int(input("Digite quantos telefones quer adicionar: "))
+    for i in range(qtd):
+        telefone = input(f"Digite o numero telefônico {i + 1}: ")
+        lista_telefone.append(telefone)
+    
+    return lista_telefone
 
 # cadastra um médico e retorna o objeto Médico criado
 def cadastro_medico():
@@ -40,19 +49,20 @@ def cadastro_paciente():
     print("############ CADASTRO DO PACIENTE ##########")
     nome = input("Digite o nome do paciente: ")
     cpf = input("Digite o cpf do paciente: ")
-    telefone = input("Digite o telefone do paciente: ")
+    lista_telefone = adicionar_telefone()
     rua = input("Digite o nome da rua: ")
     numero = input("Digite o numero da casa: ")
     bairro = input("Digite o nome do bairro: ")
 
 
-    return Paciente(nome, cpf, telefone, numero, rua, bairro)
+    return Paciente(nome, cpf, lista_telefone, numero, rua, bairro)
 
 
 #criar um atendimento e retorna o objeto Atendimento criado
 def salvar_atendimento(medico, paciente, data_do_atendimento):
 
     return Atendimento(medico, paciente, data_do_atendimento)
+
 
 # mostra todos os atendimentos na lista
 def mostrar_atendimento(lista_atendimento):
@@ -69,7 +79,7 @@ def mostrar_atendimento(lista_atendimento):
         print(f"Rua: {atendimento.paciente.rua}")
         print(f"Numero: {atendimento.paciente.numero}")
         print(f"Bairro: {atendimento.paciente.bairro}")
-        print(f"Telefone: {atendimento.paciente.telefone}")
+        print(f"Telefones: {atendimento.paciente.telefone}")
         print("--------------------------------------------------")
         print("\n")
 
@@ -90,6 +100,7 @@ def filtrar_paciente(cpf, lista_paciente):
     return None
 
 
+
 #busca e mostra os atendimentos de um médico específico baseado no CRM
 def buscar_atendimento(crm, lista_atendimento):
     for atendimento in lista_atendimento:
@@ -100,6 +111,9 @@ def buscar_atendimento(crm, lista_atendimento):
             print(f"Nome: {atendimento.paciente.nome}")
             print(f"CPF: {atendimento.paciente.cpf}")
             print(f"Rua: {atendimento.paciente.rua}")
+            print(f"Numero: {atendimento.paciente.numero}")
+            print(f"Bairro: {atendimento.paciente.bairro}")
+            print(f"Telefones: {atendimento.paciente.telefone}")
             print("--------------------------------------------------")
             print("\n")
 
